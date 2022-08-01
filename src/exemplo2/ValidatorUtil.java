26507baffdbdf67dde1bb50ff6a76b5c8900bc57
wander-luiz <wader-luiz@test.com>
2022-08-01 10:46:55 -0300

    Atividade de lancamento de excecao

--- src/exemplo2/ValidatorUtil.java
+++ src/exemplo2/ValidatorUtil.java
@@ -1,4 +1,4 @@
-package exemplo2;
+ package exemplo2;
 
 import java.time.LocalDate;
 
@@ -41,7 +41,7 @@ public class ValidatorUtil {
 
 		LocalDate plus18Years = value.plusYears(18);
 		if (plus18Years.isAfter(LocalDate.now())) {
-			//TODO Implementar o lancamento da excecao
+			throw new RuntimeException ("idade insuficiente");
 		}
 	}
 
